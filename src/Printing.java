2904170f9c0e9850eaedde481adb0c3f29dbc238
Allysa5 <allysa_adams@yahoo.com>
2017-06-19 10:02:57 -0400

    first commit

---
+++ src/Printing.java
@@ -0,0 +1,28 @@
+
+public class Printing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("+--------------------------------------+\n"+
+						"|      Java Bank ATM Receipt           |\n"+
+						"|      Wednesday, December 2, 2015     |\n"+
+						"|      ATM Location # 123              |\n"+
+						"|                                      |\n"+
+						"|                                      |\n"+
+						"|      Account Number:      1234567    |\n"+
+						"|      Customer:     John Q. Public    |\n"+
+						"|      Transaction Type:    Deposit    |\n"+
+						"|      Transaction Amount:  $500.00    |\n"+
+						"|      Account Balance:   $1,500.00    |\n"+
+						"|                                      |\n"+
+						"|      Thank you for banking with us   |\n"+
+						"|            Have a coffee day         |\n"+
+						"|                                      |\n"+
+						"+--------------------------------------+");
+		
+	}
+
+}
+
